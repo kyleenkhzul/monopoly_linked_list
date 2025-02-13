d732ca2e521d5e9d8eeebaf89f66992dbe222831
kyleenkhzul <90794184+kyleenkhzul@users.noreply.github.com>
2025-02-12 23:06:03 -0600

    implemented rolling die feature

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.Random;
 public class CircularLinkedList<T> {
     
     // Nested Node Class
@@ -17,22 +17,19 @@ public class CircularLinkedList<T> {
     // End of Nested Node Class
 
     // instance variables
-    private Node<T> head, current;
+    private Node<T> tail, current;
     
     // this function adds data to the end of the linked list
     public void append(T data) {
         Node<T> newNode = new Node<>(data);
-        if(head == null) {
-            head = newNode;
-            head.next = head;
-            current = head;
+        if(tail == null) {
+            tail = newNode;
+            tail.next = tail;
+            current = tail;
         } else {
-            Node<T> temp = head;
-            while(temp.next != head) {
-                temp = temp.next;
-            }
-            temp.next = newNode;
-            newNode.next = head;
+            newNode.next = tail.next;
+            tail.next = newNode;
+            tail = newNode;
         }
     }
 
@@ -47,6 +44,14 @@ public class CircularLinkedList<T> {
     public T currentNode() {
         return current != null ? current.data : null;
     }
+    
+    public int rollDie() {
+        Random random = new Random();
+        int firstRoll, secondRoll;
+        firstRoll = random.nextInt(6) + 1;
+        secondRoll = random.nextInt(6) + 1;
+        return firstRoll + secondRoll;
+    }
 
     public static void main(String[] args) {
         CircularLinkedList<String> monopolyBoard = new CircularLinkedList<String>();
@@ -60,9 +65,18 @@ public class CircularLinkedList<T> {
             System.err.println(e.getMessage());
         }
 
+        // Showing the program works
         for(int i = 0; i < 41; i++) {
             System.out.println(monopolyBoard.currentNode());
             monopolyBoard.step();
-          }
+        }
+
+        // Extra credit opportunity, rolling the die
+        System.out.println("\nThe current position is: " + monopolyBoard.currentNode());
+        int rolled = monopolyBoard.rollDie();
+        for(int i = 0; i < rolled; i++) {
+            monopolyBoard.step();
+        }
+        System.out.println("After moving " + rolled + " spaces. You are now on: " + monopolyBoard.currentNode());
     }
 }
